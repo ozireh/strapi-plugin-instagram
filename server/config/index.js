0f948531cda22c9e15830f2b5ad8908a1cd29202
Calvin Herizo <calvin.herizo@gmail.com>
2023-03-01 10:29:48 +0100

    add test cron

--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,6 @@
 'use strict';
 
-const cronTasks = require("./cron-tasks")
-
 module.exports = {
   default: {},
   validator() {},
-  cron: {
-    enabled: true,
-    tasks: cronTasks,
-  }
 };
--- server/register.js
+++ server/register.js
@@ -2,4 +2,15 @@
 
 module.exports = ({ strapi }) => {
   // registeration phase
+  strapi.cron.add({
+    // runs every second
+    check: {
+      task: ({ strapi }) => {
+        /* Add your own logic here */
+        console.log('check task')
+      },
+      // only run once after 10 seconds
+      options: new Date(Date.now() + 10000)
+    },
+  });
 };

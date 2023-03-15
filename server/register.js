9b9a5e86936d5a46f6f82b70a2147a875baea7a0
Calvin Herizo <calvin.herizo@gmail.com>
2023-03-15 10:35:13 +0100

    remove logs

--- server/register.js
+++ server/register.js
@@ -7,7 +7,6 @@ module.exports = ({ strapi }) => {
     check: {
       task: ({ strapi }) => {
         /* Add your own logic here */
-        console.log('check task')
       },
       // only run once after 10 seconds
       options: new Date(Date.now() + 10000)
